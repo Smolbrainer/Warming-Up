2ea3056d3b559da6eb331552e86690a4ee6f8f49
Smolbrainer <109930415+Smolbrainer@users.noreply.github.com>
2024-11-03 02:40:30 -0500

    confused whats happening

--- Beginner_Python/Simple Quiz Game.py
+++ Beginner_Python/Simple Quiz Game.py
@@ -1,7 +1,46 @@
 import re
 
-question = re.split(r'\d+', open(r'C:\Users\monfa\Documents\GitHub\Warming-Up\Beginner_Python\Simple Quiz Game Files\Questions.txt').read())
+def getChoices(questionFile):
+    choices = []
+    choiceList = []
+    for lines in questionFile:
+        lines = lines.strip()
+        if not re.match(r'^\d.*\?$', lines): # checks to see if its not a question
+            choices.append(lines)
+        else:
+            if choices: #checks to make sure choices isnt empty
+                choiceList.append(choices)
+                choices = []
+    if choices:
+        choiceList.append(choices)
+    return(choiceList)
+
+def startQuiz():
+    score = 0
+    for i in quiz:
+        print(i['Question'])
+        for k in i['Choices']:
+            print(k)
+        answer = str(input("Whats the answer? "))
+        if answer.lower() == i['Answer'].lower():
+            print(f'{i['Answer']} is Correct!')
+            score +=1
+        else:
+            print(f"Thats incorrect, The correct answer was {i['Answer']}")
+        input()
+
+    print(f"Your final grade was {score}/{len(quiz)}")
+
+questionFile = open(r'C:\Users\monfa\Documents\GitHub\Warming-Up\Beginner_Python\Simple Quiz Game Files\Questions.txt').readlines()
+questions = [line.strip() for line in questionFile if re.match(r'^\d.*\?$', line.strip())]
 answers = open(r'C:\Users\monfa\Documents\GitHub\Warming-Up\Beginner_Python\Simple Quiz Game Files\Answers.txt').read().splitlines()
 
-print (question)
-print (answers)
+choices = getChoices(questionFile)
+
+quiz = [
+]
+
+for i in range(len(questions)):
+    quiz.append(dict(Question = questions[i], Choices = choices[i], Answer = answers[i]))
+
+startQuiz()
